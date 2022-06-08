e483de5c438977d708420b84e22f006ccdb9b934
lsepu <55538804+lsepu@users.noreply.github.com>
2022-06-08 17:35:20 -0500

    Stock layout created

--- src/pages/Stock.tsx
+++ src/pages/Stock.tsx
@@ -1,9 +1,42 @@
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 const Stock = () => {
   return (
-    <Header />
-)
-}
+    <>
+      <Header />
 
-export default Stock
+      <h1 className="tab-title">Product Stock</h1>
+      <div className="container">
+        <div className="table-wrapper">
+          <table className="striped highlight table-styling centered">
+            <thead>
+              <tr>
+                <th>Product Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Min units</th>
+                <th>Max units</th>
+                <th>Provider</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Screw</td>
+                <td>A great screw to have in your store</td>
+                <td>$0.87</td>
+                <td>2</td>
+                <td>10</td>
+                <td>Pepito</td>
+                <td>5</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Stock;
