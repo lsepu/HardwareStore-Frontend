b7e93797920a2e29c21ad2dc602fae4c2b6209d2
lsepu <55538804+lsepu@users.noreply.github.com>
2022-06-07 22:15:50 -0500

    ADD: Initial routing for login and store added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
+  const AuthWrapper = (isAuthenticated: boolean) => {
+    return isAuthenticated ? (
+      <Navigate to="/store" replace />
+    ) : (
+      <Navigate to="/login" replace />
+    );
+  };
 
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={AuthWrapper(false)} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="*"
+            element={
+              <div>
+                <h2>404 Page not found</h2>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
