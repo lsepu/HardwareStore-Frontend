65266aa32e6ede53606c7eb02ff4bdf7ae983b52
lsepu <55538804+lsepu@users.noreply.github.com>
2022-06-09 00:59:51 -0500

    ADD: Form styling

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -9,17 +9,49 @@ const AddProduct = () => {
   const [productInput, setProductInput] = useState({
     name: "",
     description: "",
-    price: 0,
-    quantity: 0,
-    minUnits: 0,
-    maxUnits: 0,
+    price: "",
+    quantity: "",
+    minUnits: "",
+    maxUnits: "",
     provider: {},
   });
+
+  const [providerInput, setProviderInput] = useState({});
 
   const addNewProduct = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     e.preventDefault();
+    console.log(productInput);
+  };
+
+  const clearProductInput = () => {
+    setProductInput({
+      name: "",
+      description: "",
+      price: "",
+      quantity: "",
+      minUnits: "",
+      maxUnits: "",
+      provider: {},
+    });
+  };
+
+  const setProduct = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProductInput({
+      ...productInput,
+      [e.target.name]: [e.target.value],
+    });
+  };
+
+  const setProvider = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    provider: any
+  ) => {
+    setProductInput({
+      ...productInput,
+      provider: provider,
+    });
   };
 
   return (
@@ -33,31 +65,61 @@ const AddProduct = () => {
           <form className="col s12">
             <div className="row">
               <div className="input-field col s6">
-                <input type="text" className="validate" />
+                <input
+                  value={productInput.name}
+                  name="name"
+                  onChange={setProduct}
+                  type="text"
+                />
                 <label>Product name</label>
               </div>
               <div className="input-field col s6">
-                <input type="text" className="validate" />
+                <input
+                  value={productInput.description}
+                  type="text"
+                  name="description"
+                  onChange={setProduct}
+                />
                 <label>Description</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s6">
-                <input type="text" className="validate" />
+                <input
+                  value={productInput.price}
+                  type="text"
+                  name="price"
+                  onChange={setProduct}
+                />
                 <label>Price</label>
               </div>
               <div className="input-field col s6">
-                <input type="text" className="validate" />
+                <input
+                  value={productInput.quantity}
+                  type="text"
+                  name="quantity"
+                  onChange={setProduct}
+                />
                 <label>Quantity</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s6">
-                <input type="text" className="validate" />
+                <input
+                  value={productInput.minUnits}
+                  type="text"
+                  name="minUnits"
+                  onChange={setProduct}
+                />
                 <label>Minimum units</label>
               </div>
               <div className="input-field col s6">
-                <input type="text" className="validate" />
+                <input
+                  value={productInput.maxUnits}
+                  type="text"
+                  name="maxUnits"
+                  onChange={setProduct}
+                />
                 <label>Maximum units</label>
               </div>
             </div>
@@ -72,8 +134,15 @@ const AddProduct = () => {
                   {providers.map((provider) => (
                     <p>
                       <label>
-                        <input name={provider.idCard} type="radio" />
-                        <span>{provider.name}</span>
+                        <input
+                          name={provider.idCard}
+                          type="radio"
+                          checked={providerInput === provider}
+                          onChange={(e) => setProvider(e, provider)}
+                        />
+                        <span style={{ fontWeight: "700" }}>
+                          {provider.name}
+                        </span>
                       </label>
                     </p>
                   ))}
